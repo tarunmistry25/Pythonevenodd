00dc08078d9567ce52cee3d8dd56c2fda45959f3
Rohan Sakhale <rohansakhale@gmail.com>
2016-10-08 11:40:52 +0530

    Added functions

--- evenodd.py
+++ evenodd.py
@@ -1,6 +1,9 @@
+def isEven(number):
+    if number % 2  == 0:  
+        print("This is an even number.")  
+    else:  
+        print("This is an odd number.")  
+    
 num = int(input("Enter a number: "))  
-mod = num % 2  
-if mod > 0:  
-    print("This is an odd number.")  
-else:  
-    print("This is an even number.")  
+
+isEven(num)
